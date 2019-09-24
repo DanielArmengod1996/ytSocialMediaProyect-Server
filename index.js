d33e6d464541b07de2ee61ac044fc16f2710adc8
Daniel Armengod Sorrosal <danielarmengodsorrosal@gmail.com>
2019-09-24 17:18:28 +0200

    Subida de nueva llamada que recoge todas los municipios pertenecientes a la provicia metida por parámetro en una segunda llamada al servidor de el-tiempo.net

--- index.js
+++ index.js
@@ -1,103 +1,161 @@
-// API: https://www.pue.es/cursos/salesforce/tech-talk-cta
-//var url = 'https://www.el-tiempo.net/api/json/v1/provincias';
-
-var option1 = 
-{
-  url : 'https://www.el-tiempo.net/api/json/v1/provincias',
-  headers: {
-    'User-Agent':'request'
-  }
-};
-
-// init node-modules
-const express = require('express');
-const request = require('request');
+  // API: https://www.pue.es/cursos/salesforce/tech-talk-cta
+  //var url = 'https://www.el-tiempo.net/api/json/v1/provincias';
+  var CODPROV = '';
+  var ID = '';
 
 
 
-//instances
-const app = express();
+
+  var option3 = 
+  {
+    url : `https://www.el-tiempo.net/api/json/v1/provincias/${CODPROV}/municipios/${ID}/weather`,
+    headers: {
+      'User-Agent':'request'
+    }
+  };
+
+  // init node-modules
+  const express = require('express');
+  const request = require('request');
 
 
 
-/**
- * @description webservice method type 'get'
- */
-app.get('/getNames', (req, wsRes) => {
-  doGetRequest(option1).then(function(data){
-    const finalResponse = data;
-    console.log(finalResponse);
-    const provincia = req.query.provincia;
-    const wrappedResponse = prepareResponse( finalResponse, provincia );
-    const responseBody = JSON.stringify(wrappedResponse);
-    wsRes.setHeader('Access-Control-Allow-Origin', '*');
-    wsRes.setHeader('content-type', 'application/json');
-    
-    wsRes.send( responseBody );
-  });
-
-});
-
-
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-/**
- * @description function that is established to listen in port 8080
- */
-app.listen(PORT, () => {
-  console.log('The server has been launched');
-  console.log('============================');
-  console.log('|========= HELLO ==========|');
-  console.log('============================');
-  console.log(`Server listening on port ${PORT}...`);
-});
+  //instances
+  const app = express();
 
 
 
-/**
- * @description prepare response to client
- * @param {*} responseBody 
- * @param {*} res 
- */
-function prepareResponse(jsonText, nombreProvincia){
-  var lstCodigoProvincia = new Array();
-  var responseBody = JSON.parse( jsonText );
-  if( responseBody ){
-    responseBody.forEach(element => {
-      if( element.NOMBRE_PROVINCIA.toUpperCase().includes( nombreProvincia.toUpperCase() ) ){
-        lstCodigoProvincia.push( new WrpResponse( element.CODPROV, element.NOMBRE_PROVINCIA ) );
+  /**
+   * @description webservice method type 'get'
+   */
+  app.get('/getWeather', (req, wsRes) => {
+    var responseBody;
+    var option1 = 
+    {
+      url : 'https://www.el-tiempo.net/api/json/v1/provincias',
+      headers: {
+        'User-Agent':'request'
       }
-    });
+    };
 
-  }
-  return lstCodigoProvincia;
+    doGetRequest(option1).then(function(dataOption1){
 
-}
+      /* request for the province code*/
+      const finalResponse = dataOption1;
+      //console.log(finalResponse);
+      const provincia = req.query.provincia;
+      responseBody = prepareResponse( finalResponse, provincia );
+      //return wrappedResponse;
+    }).then(function(data){
+      // we instance the codprov to get the info from the weather
+      
+      CODPROV = responseBody[0].codigoProvincia;
 
+      var option2 = 
+      {
+        url : `https://www.el-tiempo.net/api/json/v1/provincias/${CODPROV}/municipios`,
+        headers: {
+          'User-Agent':'request'
+        }
+      };
 
-class WrpResponse{
-  constructor(codigoProvincia, nombreProvincia){
-    this.codigoProvincia = codigoProvincia;
-    this.nombreProvincia = nombreProvincia;
-  }
-}
+      /* call to get the response with the cod prov */
+      doGetRequest(option2).then(function(dataOption2){
+        // request to get the id from the municipie
+        //console.log('DATA OPTION 2 : ' + dataOption2);
+        console.log(dataOption2);
+        responseBody = extractAllDataFromProvince(dataOption2);
 
+      }).then(function(data){
+        
+        /* final response */
+        wsRes.setHeader('Access-Control-Allow-Origin', '*');
+        wsRes.setHeader('content-type', 'application/json');
+        //console.log('responseBody :: ' + responseBody);
+        wsRes.send( responseBody );
 
+        doGetRequest(option3).then(function(dataOption3){
+          // request to get the weather from the province with the id
+          //console.log('DATA OPTION 3: ' + dataOption3 );
+        });
 
-
-
-function doGetRequest(option){
-  return new Promise(function(resolve, reject){
-  
-  request.get(option, function(err, resp, body){
-      if (err) {
-        reject(err);
-      } else {
-        resolve(body);
-      }
-
+      });
     });
 
   });
- 
-}
+
+
+  // Listen to the App Engine-specified port, or 8080 otherwise
+  const PORT = process.env.PORT || 8080;
+  /**
+   * @description function that is established to listen in port 8080
+   */
+  app.listen(PORT, () => {
+    console.log('The server has been launched');
+    console.log('============================');
+    console.log('|========= HELLO ==========|');
+    console.log('============================');
+    console.log(`Server listening on port ${PORT}...`);
+  });
+
+
+
+  /**
+   * @description prepare response to client
+   * @param {*} responseBody 
+   * @param {*} res 
+   */
+  function prepareResponse(jsonText, nombreProvincia){
+    var lstCodigoProvincia = new Array();
+    var responseBody = JSON.parse( jsonText );
+    if( responseBody ){
+      responseBody.forEach(element => {
+        if( element.NOMBRE_PROVINCIA.toUpperCase().includes( nombreProvincia.toUpperCase() ) ){
+          lstCodigoProvincia.push( new WrpResponse( element.CODPROV, element.NOMBRE_PROVINCIA ) );
+        }
+      });
+
+    }
+    return lstCodigoProvincia;
+
+  }
+
+
+  class WrpResponse{
+    constructor(codigoProvincia, nombreProvincia){
+      this.codigoProvincia = codigoProvincia;
+      this.nombreProvincia = nombreProvincia;
+    }
+  }
+
+
+
+
+
+  function doGetRequest(option){
+    return new Promise(function(resolve, reject){
+      request.get(option, function(err, resp, body){
+        if (err) {
+          reject(err);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+  }
+
+
+  //50
+  //to
+  //8120
+
+  function extractAllDataFromProvince(responseBody){
+    console.log('entro a ejecutar este bucle');
+    for (var i = 40; i < 8120; i++) {
+      var str = i.toString();
+      responseBody = responseBody.replace(`"${str}":`, '');
+    }
+    responseBody = responseBody.replace('{{', '[{');
+    responseBody = responseBody.replace('}}', '}]');
+    return responseBody;
+  }
